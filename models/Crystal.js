2a8091a02c545592cb741bc51a07617edcf7cec2
OttoBisno <ottobisno444@gmail.com>
2022-11-21 19:18:35 -0800

    Updated User and Crystal models

---
+++ models/Crystal.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Crystal extends Model {}
+
+Crystal.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        hardness: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modeName: 'crystal'
+    }
+);
+
+module.exports = Crystal;
