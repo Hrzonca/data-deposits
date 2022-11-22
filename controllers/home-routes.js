d52ab20d8c5019c355908c0da3958a98d481e0f1
Haleigh Rzonca <hgielah@gmail.com>
2022-11-21 19:14:42 -0800

    Working on crystal routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,22 @@ const router = require('express').Router();
 const { User, Crystal } = require('../models');
 const withAuth = require('../utils/auth.js');
 
+//TODO: profile to make your own posts
+
+//TODO: login page 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+//This route is for people who have logged in and can see the rare crystals. Not sure what the endpoint should be. 
 // Use withAuth middleware to prevent access to route
-router.get('/rarecrystals', withAuth, async (req, res) => {
+router.get('/Crystals', withAuth, async (req, res) => {
     try {
       // Find the logged in user based on the session ID
       const userData = await User.findByPk(req.session.user_id, {
