aa0950679ed922cfbc7fe8ea31d2a3698119614a
OttoBisno <ottobisno444@gmail.com>
2022-11-28 18:51:03 -0800

    Updated crystal description type to text

--- models/Crystal.js
+++ models/Crystal.js
@@ -16,7 +16,7 @@ Crystal.init(
             allowNull: false
         },
         description: {
-            type: DataTypes.STRING
+            type: DataTypes.TEXT
         },
         price: {
             type: DataTypes.DECIMAL,
